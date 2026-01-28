0f90a43c4f42817714933bb61768a8ee10cf974b
Wout <wout.struys@outlook.com>
2026-01-28 10:42:49 +0100

    Fix CORS origins for credentialed requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"], 
+    allow_origins=settings.CORS_ORIGINS,
     allow_methods=["*"],
     allow_headers=["*"],
 )
